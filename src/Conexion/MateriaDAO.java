d6f48c475e0a6c8a504ac7aa514e05594b28f098
JNZader <javierzader@hotmail.com>
2023-09-12 19:11:41 -0300

    A VER...
    QUE PASO ACAAA!!!

--- src/Conexion/MateriaDAO.java
+++ src/Conexion/MateriaDAO.java
@@ -1,5 +1,155 @@
 package Conexion;
 
-public class MateriaDAO {
+import Entidades.Materia;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.JOptionPane;
+
+public class MateriaDAO {
+    private Connection con;
+
+    public MateriaDAO(Connection con) {
+        
+        try {
+            con= Conexion.getConnection();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex);
+        }
+    }
+    
+    public void guardarMateria(Materia materia){
+        String sql = "INSERT INTO materia(año,nombre,estado)VALUES(?,?,?)";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            
+            ps.setInt(1, materia.getAño());
+            ps.setString(2, materia.getNombre());
+            ps.setBoolean(3, materia.isEstado());
+            
+            ps.executeUpdate();
+            
+            ResultSet rs = ps.executeQuery();
+            
+            
+            if (rs.next()) {
+                materia.setIdMateria(rs.getInt("idMateria"));
+                JOptionPane.showMessageDialog(null,"Materia añadida con exito");
+            }
+            ps.close();
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla materia"+ex.getMessage());
+        }
+        
+    }
+    
+    public Materia BuscarMateria(int id){
+        Materia materia=null;
+        String sql ="SELECT año,nombre FROM materia WHERE idMateria=? estado=1";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs =ps.executeQuery();
+            if (rs.next()) {
+                materia = new Materia();
+                
+                materia.setIdMateria(id);
+                materia.setAño(rs.getInt("año"));
+                materia.setNombre(rs.getString("nombre"));
+                materia.setEstado(true);
+                
+                
+            }else{
+                JOptionPane.showMessageDialog(null, "No existe la materia");
+            }
+            
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla materia"+ex.getMessage());
+        }
+        
+        return materia;
+    }
+    
+    public void modificarMateria(Materia materia){
+        String sql = "UPDATE materia SET año=?,nombre=? WHERE idMateria=?";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, materia.getAño());
+            ps.setString(2, materia.getNombre());
+            
+            int fila=ps.executeUpdate();
+            
+            if (fila==1) {
+                JOptionPane.showMessageDialog(null, "Modificado exitosamente");
+            }else{
+                JOptionPane.showMessageDialog(null, "La materia no existe");
+            }
+           
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla materia"+ex.getMessage());
+        }
+        
+    }
+    
+    public void eliminarMateria(int id){
+        String sql="UPDATE materia SET estado=0 WHERE idMateria=?";
+        
+        try {
+            PreparedStatement ps= con.prepareStatement(sql);
+            ps.setInt(1, id);
+            
+            int fila =ps.executeUpdate();
+            
+            if (fila==1) {
+                JOptionPane.showMessageDialog(null, " Se eliminó la materia");
+            }else{
+                JOptionPane.showMessageDialog(null, " La materia no existe");
+            }
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla materia"+ex.getMessage());
+        }
+        
+    }
+    
+    public List<Materia> listarMaterias(){
+        ArrayList<Materia>materias=new ArrayList<>();
+        String sql = "SELECT * FROM materia WHERE estado=1";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs =ps.executeQuery();
+            
+            while (rs.next()) {
+                Materia materia = new Materia();
+                
+                materia.setIdMateria(rs.getInt("idMateria"));
+                materia.setAño(rs.getInt("año"));
+                materia.setNombre(rs.getString("nombre"));
+                materia.setEstado(true);
+                materias.add(materia);
+                
+                
+            }
+            ps.close();
+            
+        } catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null, "Error al acceder a la tabla materia"+ex.getMessage());
+        }
+        
+        return materias;
+    }
+    
+    
 }
--- src/Entidades/Alumno.java
+++ src/Entidades/Alumno.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 public class Alumno {
     private int idAlumno;
+    private int dni;
     private String nombre, apellido;
     private LocalDate fechaNacimiento;
     private boolean estado;
@@ -11,15 +12,17 @@ public class Alumno {
     public Alumno() {
     }
 
-    public Alumno(String nombre, String apellido, LocalDate fechaNacimiento, boolean estado) {
+    public Alumno(int dni,String nombre, String apellido, LocalDate fechaNacimiento, boolean estado) {
+        this.dni=dni;
         this.nombre = nombre;
         this.apellido = apellido;
         this.fechaNacimiento = fechaNacimiento;
         this.estado = estado;
     }
 
-    public Alumno(int idAlumno, String nombre, String apellido, LocalDate fechaNacimiento, boolean estado) {
+    public Alumno(int idAlumno,int dni, String nombre, String apellido, LocalDate fechaNacimiento, boolean estado) {
         this.idAlumno = idAlumno;
+        this.dni=dni;
         this.nombre = nombre;
         this.apellido = apellido;
         this.fechaNacimiento = fechaNacimiento;
@@ -33,6 +36,15 @@ public class Alumno {
     public void setIdAlumno(int idAlumno) {
         this.idAlumno = idAlumno;
     }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+    
 
     public String getNombre() {
         return nombre;
